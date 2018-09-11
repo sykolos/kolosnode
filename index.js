56bce3d17c30eb8a2651979df8b07c360134d1ce
sykolos <43166863+sykolos@users.noreply.github.com>
2018-09-11 10:45:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("<html><head><title>leszopolip</title></head><body><center><h1 style='color:red'>Leszopolip</h1></center></body></html>");
-});
-
+var fs = require('fs');
 var port = process.env.PORT || 1337;
-server.listen(port);
+http.createServer(function (req, res) {
+	fs.readFile('ize.html', function(err, data) {
+	res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write(data);
+	res.end();
 
-console.log("Server running at http://localhost:%d", port);
+}).listen(port);
