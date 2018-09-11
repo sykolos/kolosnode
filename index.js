6fab72177931362c362f986c4bb4782e1de69604
sykolos <43166863+sykolos@users.noreply.github.com>
2018-09-11 10:48:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
 	fs.readFile('ize.html', function(err, data) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.writeHead(200, {"Content-Type": "text/html"});
     res.write(data);
 	res.end();
     });
